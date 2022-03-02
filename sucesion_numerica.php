f90f538d171f2bd959f65674a4986a8498dc0323
Jose Alfredo Dzul Hau <70233261+JAlfredoDzulHau@users.noreply.github.com>
2022-03-02 11:38:01 -0600

    Update sucesion_numerica.php

--- sucesion_numerica.php
+++ sucesion_numerica.php
@@ -1,9 +1,8 @@
 <?php
    
-
-    
+    //La formula que sigue es: (n2 + n)/2
     for ($i = 1; $i<=20; $i++) {
-        $potencia = 2**$i;
+        $potencia = $i**2;
         $suma = ($potencia + $i);
         $resultado = ($suma / 2);
         echo $resultado, '<br>';
